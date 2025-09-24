e22ba0fd6ff92cd32f7b24d16693dfb41a8ce732
Matěj Kotas <matej@Matejs-MacBook-Pro-2.local>
2025-09-23 18:30:34 -0700

    Refactor render function

--- track.py
+++ track.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 # Class to store track data with some utility functions
 # left and right must have the same length and be aligned e.g. the first point in left has to be across from the first point in right
@@ -12,21 +13,25 @@ class Track:
         assert self.left_points.shape[1] == 2, "Points must have 2 numbers per point"
         assert self.left_points.shape == self.right_points.shape, "Left and right points must have the same shape"
 
-    def render(self, path=None, rotate=False):
+    def render(self, path=None, rotate=False, save=False):
         plt.clf()
-        
-        if not rotate:
-            plt.plot(self.left_points.T[0], self.left_points.T[1], "b.-", markersize=0, linewidth=1)
-            plt.plot(self.right_points.T[0], self.right_points.T[1], "r.-", markersize=0, linewidth=1)
 
-            if not path is None:
-                plt.plot(path.T[0], path.T[1], "r.-", markersize=0, linewidth=1)
+        start = np.stack((self.left_points[0], self.right_points[0]))
+        
+        self.render_helper(start, rotate, "g.-")
+        self.render_helper(self.left_points, rotate, "b.-")
+        self.render_helper(self.right_points, rotate, "r.-")
+
+        if not path is None:
+            self.render_helper(path, rotate, "m.-")
+
+        if save:
+            plt.savefig(f"figures/fig.png", dpi=300)
+        plt.show()
+
+    def render_helper(self, points, rotate, style):
+        if not rotate:
+            plt.plot(points.T[0], points.T[1], style, markersize=0, linewidth=1)
 
         else:
-            plt.plot(self.left_points.T[1], -self.left_points.T[0], "b.-", markersize=0, linewidth=1)
-            plt.plot(self.right_points.T[1], -self.right_points.T[0], "r.-", markersize=0, linewidth=1)
-
-            if not path is None:
-                plt.plot(path.T[1], -path.T[0], "r.-", markersize=0, linewidth=1)
-
-        plt.show()
+            plt.plot(points.T[1], -points.T[0], style, markersize=0, linewidth=1)
